c3fa87da2de498c8ac0d835d9365f3e610ea39bd
stephanyTF <stephany.lam@codepath.org>
2026-09-17 12:03:23 -0700

    test: Part 3 Added tests for session_rating weird inputs

--- test_scoring.py
+++ test_scoring.py
@@ -13,5 +13,17 @@ def test_session_rating_boundary_90_is_great():
 def test_session_rating_boundary_80_is_good():
     assert session_rating(80) == "Good"
 
+#Part 3? Test session_rating() handling bad input.
+def test_session_rating_negative_score():
+    assert session_rating(-10) == "Skip" #works
+
+
+def test_session_rating_boundary_over_100():
+    assert session_rating(1000) == "Great" #works
+
+
+def test_session_rating_boundary_decimal_is_good():
+    assert session_rating(87.5) == "Good"  #can't take decimals since expects ints, TODO: convert to int or accept floats?
+
 # TODO: add at least one more test, e.g. a boundary case for "Skip" (a score
 # of 59) or the exact boundary for "Good" (a score of 80).
